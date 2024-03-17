4e778eb98d81b527d5dc9ae15edbd5872cc2f4dd
Virgil <virgilsambo@gmail.com>
2024-03-17 11:08:27 +0100

    add base endpoint and query parameters

---
+++ app/src/main/java/com/maxdoro/employer/common/Constants.kt
@@ -0,0 +1,9 @@
+package com.maxdoro.employer.common
+
+object Constants {
+    const val BASE_URL = "https://cba.kooijmans.nl/CBAEmployerservice.svc/rest/"
+    const val QUERY_PARAM_FILTER = "filter"
+    const val QUERY_PARAM_MAX_ROWS = "maxRows"
+    const val QUERY_PARAM_EMPLOYER_ID = "employerId"
+    const val MAXDORO_DATABASE_NAME = "Maxdoro.db"
+}
