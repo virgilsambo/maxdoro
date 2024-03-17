89012f2e7aa4f2a4c2c3fef74700727f10cd6895
Virgil <virgilsambo@gmail.com>
2024-03-17 11:19:22 +0100

    implement Employer repository and handling api calls

---
+++ app/src/main/java/com/maxdoro/employer/data/EmployerNetworkDataSourceImpl.kt
@@ -0,0 +1,17 @@
+package com.maxdoro.employer.data
+
+import com.maxdoro.employer.model.Employer
+import retrofit2.Response
+import javax.inject.Inject
+
+class EmployerNetworkDataSourceImpl @Inject constructor(
+    private val maxdoroApi: MaxdoroApi
+):EmployerNetworkDataSource {
+    override suspend fun getEmployerSearchResults(searchQuery: String): Response<List<Employer>> {
+        return maxdoroApi.getEmployerSearchResults(searchQuery = searchQuery, maxRows = 100)
+    }
+
+    override suspend fun getEmployerDetail(employerId: Int): Response<Employer> {
+        return maxdoroApi.getEmployerDetail(employerId = employerId)
+    }
+}
---
+++ app/src/main/java/com/maxdoro/employer/data/EmployerRepositoryImpl.kt
@@ -0,0 +1,42 @@
+package com.maxdoro.employer.data
+
+import android.util.Log
+import com.maxdoro.employer.common.Result
+import com.maxdoro.employer.di.IoDispatcher
+import com.maxdoro.employer.model.Employer
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class EmployerRepositoryImpl @Inject constructor(
+    private val networkDataSource: EmployerNetworkDataSource,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+) : EmployerRepository {
+
+    override suspend fun getEmployerSearchResults(searchQuery: String): Result<List<Employer>> {
+        return try {
+            withContext(ioDispatcher) {
+                val response = networkDataSource.getEmployerSearchResults(searchQuery = searchQuery)
+                val body = response.body()
+
+                if (response.isSuccessful && !body.isNullOrEmpty()) {
+                    Result.Success(body)
+                } else {
+                    Log.e(
+                        "Result",
+                        "GET SearchResult unsuccessful retrofit response ${response.message()}"
+                    )
+                    Result.Error("Can't get search results")
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("Result", "GET SearchResult error ${e.message}")
+            Result.Error(" results")
+        }
+    }
+
+    override suspend fun getEmployerDetail(employerId: Int): Result<Employer> {
+        return Result.Error("Can't get search results")
+    }
+
+}
