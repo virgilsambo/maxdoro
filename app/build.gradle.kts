dffac42934afbe05814f0988bfef002ae306de51
Virgil <virgilsambo@gmail.com>
2024-03-17 11:50:14 +0100

    build.gradle.kts kotlinCompilerExtensionVersion update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.9"
     }
     packaging {
         resources {
